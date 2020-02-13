0f880d4d5ecf7c379cbb891c69fe8252aaa0f46b
James Liu <jaliu@zendesk.com>
2020-02-13 16:42:05 +1100

    fix keymap

--- keyboards/kyria/keymaps/disposedtrolley/keymap.c
+++ keyboards/kyria/keymaps/disposedtrolley/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRAVE,                KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQUAL,
       MT(MOD_LCTL, KC_BSPC),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_TAB,                  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   LT(_RAISE, KC_ESC),     KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                       KC_LGUI, KC_DEL, MT(MOD_LALT, KC_ENT), LT(_LOWER, KC_SPC), LT(_LOWER, KC_ENT),     KC_ENT, KC_RSFT, KC_SPC,  KC_BSPC, KC_RALT
+                                               KC_LGUI, KC_LALT, KC_LGUI, KC_ESC, LT(_LOWER, KC_ENT),     KC_ENT, KC_SPC,  KC_BSPC, KC_RSFT, KC_RALT
     ),
 /*
  * Lower Layer: Symbols
